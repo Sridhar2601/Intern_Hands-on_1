e0a0cb5eaaa52eaf7fa6123514fcd8263f257da4
Sridhar2601 <sridharchml@gmail.com>
2025-09-09 19:24:47 +0530

    mbl

--- src/Fox/Nav.js
+++ src/Fox/Nav.js
@@ -1,39 +1,67 @@
-import './CSS/Nav.css'
+import './CSS/Nav.css';
+
 export default function Nav() {
     return (
         <div className='main-one'>
-            <div class="nb">
-                <div> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4_JXGwMpPe3v-Qx-jVzxo2MliXPDNVbco5A&s" height="60px"
-                    width="300px" alt="company logo"></img></div>
-                <div className='d-flex'>
-                    <div style={{ alignContent: 'center' }}><i class="fa-solid fa-paper-plane" style={{ color: 'orangered' }}></i></div>
-                    <div className='mt-3'>
-                        <p className='ps-2'><b>Email</b><br />foxuniversity@org</p>
-                    </div>
+            {/* Top Info Bar */}
+            <div className="nb d-flex justify-content-between align-items-center flex-wrap p-2">
+                <div>
+                    <img 
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4_JXGwMpPe3v-Qx-jVzxo2MliXPDNVbco5A&s"
+                        height="60px"
+                        width="200px"
+                        alt="company logo"
+                    />
                 </div>
-                <div className='d-flex'>
-                    <div style={{ alignContent: 'center' }}><i class="fa-solid fa-phone" style={{ color: 'orangered' }}></i></div>
-                    <div className='mt-3'>
-                        <p className='ps-2'><b>Call</b><br />Call Us : +91-8778204667</p>
-                    </div>
+                <div className="d-flex align-items-center m-2">
+                    <i className="fa-solid fa-paper-plane me-2" style={{ color: 'orangered' }}></i>
+                    <p className="mb-0">
+                        <b>Email</b><br />foxuniversity@org
+                    </p>
                 </div>
-                <div><button className='btn btn-success' style={{ backgroundColor: 'orangered',border:'none'}}>Apply Now</button></div>
+                <div className="d-flex align-items-center m-2">
+                    <i className="fa-solid fa-phone me-2" style={{ color: 'orangered' }}></i>
+                    <p className="mb-0">
+                        <b>Call</b><br />+91-8778204667
+                    </p>
+                </div>
+                <div className="m-2">
+                    <button className="btn btn-success" style={{ backgroundColor: 'orangered', border: 'none' }}>
+                        Apply Now
+                    </button>
+                </div>
             </div>
-            <nav className='d-flex'>
-                <div class="n container bg-dark p-3">
-                    <li> <a href="/">Home</a></li>
-                    <li> <a href="/About">About us</a></li>
-                    <li> <a href="/Course">Courses</a></li>
-                    <li> <a href="/Staff">Staff</a></li>
-                    <li> <a href="/Blog">Blog</a></li>
-                    <li> <a href="/Contact">Contact us</a></li>
 
-                    <form class="d-flex" role="search" style={{ paddingLeft: '340px' }}>
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button class="btn btn-dark" type="submit">Search</button>
-                    </form>
+            {/* Navbar */}
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container">
+                    {/* Brand (optional) */}
+                    <a className="navbar-brand" href="/">Fox University</a>
+
+                    {/* Toggler for mobile */}
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu" aria-controls="navMenu" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    {/* Collapsible Menu */}
+                    <div className="collapse navbar-collapse" id="navMenu">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
+                            <li className="nav-item"><a className="nav-link" href="/About">About us</a></li>
+                            <li className="nav-item"><a className="nav-link" href="/Course">Courses</a></li>
+                            <li className="nav-item"><a className="nav-link" href="/Staff">Staff</a></li>
+                            <li className="nav-item"><a className="nav-link" href="/Blog">Blog</a></li>
+                            <li className="nav-item"><a className="nav-link" href="/Contact">Contact us</a></li>
+                        </ul>
+
+                        {/* Search Box */}
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-light" type="submit">Search</button>
+                        </form>
+                    </div>
                 </div>
             </nav>
         </div>
-    )
+    );
 }
